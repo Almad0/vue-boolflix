0949bd06b7a1592ea4d2f0e04017f9a37c51c209
Alessandro <internicola.alessandro@gmail.com>
2021-01-12 13:07:54 +0100

    Html/css base + test collegamento VueJs
    Creata (su base dell'esercizio precedente) un layout di base, collegato il bottone al Vue per test

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,22 @@ let root = new Vue({
   el: "#root",
 
   data: {
+    test: 12
+  },
+
+  computed: {
 
   },
 
   methods: {
+    searchApi: function(){
+      console.log(this.test);
+    }
+  },
+
+  mounted(){
+
+  }
 
 });
 
