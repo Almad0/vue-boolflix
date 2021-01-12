bbf53208f456700b51dec29fc850fff0aad1d1db
Alessandro <internicola.alessandro@gmail.com>
2021-01-12 20:04:35 +0100

    Milestone 2 - 80%
    Sistemato il problema delle stelline, usato un database esterno per l'immagine delle bandiere (usata la notazione es6 per il src)

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,15 +14,19 @@ let root = new Vue({
   methods: {
     searchApi: function(){
       console.log(this.search);
-
-      axios.get("https://api.themoviedb.org/3/search/movie?api_key=101e0cd873c9360f9bc83e1b717d2943&language=en-US&query=" + this.search + "&page=1&include_adult=false")
+      axios.get("https://api.themoviedb.org/3/search/movie?api_key=101e0cd873c9360f9bc83e1b717d2943&language=it-IT&query=" + this.search + "&page=1&include_adult=false")
       .then(resp => {
         console.log(resp.data.results);
         this.moviesData=(resp.data.results);
         this.search = null
+        this.moviesData.forEach(item => {
+          let voto = Math.ceil(item.vote_average / 2);
+          item.vote_average = voto
+          if (item.original_language == "en"){
+            item.original_language = "gb";
+          }
+        });
       })
-
-      console.log(this.moviesData);
     }
   },
 
