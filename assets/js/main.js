52919aa70b2a9f51d8dfa859f8ef795a388be65b
Alessandro <internicola.alessandro@gmail.com>
2021-01-12 15:59:17 +0100

    milestone 1 completa
    Usato un valore search collegato all'input per filtrare la richiesta all'API tramite la query

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ let root = new Vue({
   el: "#root",
 
   data: {
-    test: 12
+    moviesData: [],
+    search: null,
   },
 
   computed: {
@@ -12,7 +13,16 @@ let root = new Vue({
 
   methods: {
     searchApi: function(){
-      console.log(this.test);
+      console.log(this.search);
+
+      axios.get("https://api.themoviedb.org/3/search/movie?api_key=101e0cd873c9360f9bc83e1b717d2943&language=en-US&query=" + this.search + "&page=1&include_adult=false")
+      .then(resp => {
+        console.log(resp.data.results);
+        this.moviesData=(resp.data.results);
+        this.search = null
+      })
+
+      console.log(this.moviesData);
     }
   },
 
